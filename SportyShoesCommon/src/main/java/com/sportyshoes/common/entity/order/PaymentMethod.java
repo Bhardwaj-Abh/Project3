bf980b420e0bd97fe0f7dcd19ff912f497cfd5ce
Bhardwaj-Abh <95326088+Bhardwaj-Abh@users.noreply.github.com>
2022-05-12 22:11:35 +0530

    Add files via upload

---
+++ SportyShoesCommon/src/main/java/com/sportyshoes/common/entity/order/PaymentMethod.java
@@ -0,0 +1,5 @@
+package com.sportyshoes.common.entity.order;
+
+public enum PaymentMethod {
+	COD, CREDIT_CARD
+}
---
+++ SportyShoesCommon/src/main/java/com/sportyshoes/common/exception/CategoryNotFoundException.java
@@ -0,0 +1,9 @@
+package com.sportyshoes.common.exception;
+
+public class CategoryNotFoundException extends Exception {
+
+	public CategoryNotFoundException(String message) {
+		super(message);
+	}
+
+}
